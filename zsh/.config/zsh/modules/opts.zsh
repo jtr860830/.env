dffc4762c6f3941b962e2c8f59e802d2941f1709
Josh Hsieh <jtr860830@gmail.com>
2023-10-05 23:15:52 +0800

    feat: update zsh history configuration

--- zsh/.config/zsh/modules/opts.zsh
+++ zsh/.config/zsh/modules/opts.zsh
@@ -1,3 +1,40 @@
+# Enable additional glob operators. (Globbing = pattern matching)
+# https://zsh.sourceforge.io/Doc/Release/Expansion.html#Filename-Generation
+setopt EXTENDED_GLOB
+
+# Tell zsh where to store history.
+# $VENDOR and $OSTYPE let us check what kind of machine we're on.
+local icloud=~/Library/Mobile\ Documents/com\~apple\~CloudDocs
+if [[ -d $icloud && $VENDOR == apple ]]; then
+  # If using iCloud on macOS, store it there, so it syncs across multiple Macs.
+  HISTFILE=$icloud/zsh/history
+
+  # Sometimes (probably due to concurrency issues), when the histfile is kept in
+  # iCloud, it is empty when Zsh starts up. However, there should always be a
+  # backup file we can copy.
+
+  # Move the largest "$HISTFILE <number>" file to $HISTFILE.
+  # \ escapes/quotes the space behind it.
+  # (O):  Sort descending.
+  # (OL): Sort by size, descending.
+  local -a files=( $HISTFILE(|\ <->)(OL) )
+  [[ -r $files[1] ]] &&
+      mv $files[1] $HISTFILE
+else
+  # := assigns the variable and then substitutes the expression with its value.
+  HISTFILE=${XDG_DATA_HOME:=~/.local/share}/zsh/history
+fi
+
+# Just in case: If the parent directory doesn't exist, create it.
+[[ -d $HISTFILE:h ]] ||
+    mkdir -p $HISTFILE:h
+
+# Max number of entries to keep in history file.
+SAVEHIST=$(( 100 * 1000 ))      # Use multiplication for readability.
+
+# Max number of history entries to keep in memory.
+HISTSIZE=$(( 1.2 * SAVEHIST ))  # Zsh recommended value
+
 # History optimization
 setopt append_history         # Allow multiple sessions to append to one Zsh command history.
 setopt extended_history       # Show timestamp in history.
@@ -9,6 +46,7 @@ setopt hist_ignore_space      # Do not record an Event Starting With A Space.
 setopt hist_reduce_blanks     # Remove superfluous blanks from history items.
 setopt hist_save_no_dups      # Do not write a duplicate event to the history file.
 setopt hist_verify            # Do not execute immediately upon history expansion.
+setopt hist_fcntl_lock        # Use modern file-locking mechanisms, for better safety & performance.
 setopt inc_append_history     # Write to the history file immediately, not when the shell exits.
 setopt share_history          # Share history between different instances of the shell.
 
