a469049b164b704d125f6a97814a9a2e6b6e9e6c
Josh Hsieh <jtr860830@gmail.com>
2023-10-10 04:15:51 +0800

    feat: add zoxide to zsh

--- zsh/.config/zsh/modules/znap.zsh
+++ zsh/.config/zsh/modules/znap.zsh
@@ -5,11 +5,15 @@ ZPLUGINS_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zsh/plugins"
     https://github.com/marlonrichert/zsh-snap.git $ZPLUGINS_HOME/znap
 source $ZPLUGINS_HOME/znap/znap.zsh
 
+# setup plugins
 znap install zsh-users/zsh-completions
+
 znap source romkatv/powerlevel10k
 znap source zdharma-continuum/fast-syntax-highlighting
 znap source zsh-users/zsh-autosuggestions
 znap source marlonrichert/zsh-autocomplete
 znap source marlonrichert/zsh-edit
 znap source marlonrichert/zcolors
+znap source ajeetdsouza/zoxide
+
 znap eval zcolors "zcolors ${(q)LS_COLORS}"
