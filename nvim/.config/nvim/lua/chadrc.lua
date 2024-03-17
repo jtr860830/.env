50dbd88fbfc7e1883f90592ef8b4452a229ec3bb
Josh Hsieh <dev@josh-hsieh.tw>
2024-03-17 21:30:31 +0800

    chore: update neovim configuration

--- nvim/.config/nvim/lua/chadrc.lua
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -2,6 +2,10 @@ local M = {}
 
 M.ui = {
   theme = "doomchad",
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
   statusline = {
     theme = "vscode_colored",
   },
