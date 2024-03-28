dfb3a60914599e2b6ab3ed48129d24020bf525df
Josh Hsieh <dev@josh-hsieh.tw>
2024-03-29 01:49:12 +0800

    feat(neovim): add different js runtime support

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,4 +1,5 @@
-local configs = require("nvchad.configs.lspconfig")
+local configs = require "nvchad.configs.lspconfig"
+local lspconfig = require "lspconfig"
 
 local on_attach = configs.on_attach
 local on_init = configs.on_init
@@ -9,18 +10,24 @@ local servers = {
   clangd = {
     cmd = {
       "clangd",
-      "--offset-encoding=utf-16"
+      "--offset-encoding=utf-16",
     },
+  },
+  denols = {
+    root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
   },
   gopls = {},
   pyright = {},
   rust_analyzer = {},
-  tsserver = {},
+  tsserver = {
+    root_dir = lspconfig.util.root_pattern "package.json",
+    single_file_support = false,
+  },
 }
 
 for name, config in pairs(servers) do
   config.on_init = on_init
   config.on_attach = on_attach
   config.capabilities = capabilities
-  require("lspconfig")[name].setup(config)
+  lspconfig[name].setup(config)
 end
