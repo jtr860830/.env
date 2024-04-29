13d1c40d76875db3e6c845802946848428632120
Josh Hsieh <dev@josh-hsieh.tw>
2024-04-29 16:03:49 +0800

    feat: update wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -12,6 +12,7 @@ end
 
 -- This is where you actually apply your config choices
 config.window_decorations = "RESIZE"
+config.cursor_blink_rate = 0
 config.tab_bar_at_bottom = true
 config.hide_tab_bar_if_only_one_tab = true
 config.color_scheme = "onedarkpro_onedark"
@@ -20,6 +21,13 @@ config.font = wezterm.font("MonaspiceNe NF")
 config.font_size = 14
 config.harfbuzz_features =
   { "calt", "liga", "dlig", "zero", "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08" }
+config.keys = {
+  {
+    key = "f",
+    mods = "CTRL|OPT|CMD",
+    action = wezterm.action.ToggleAlwaysOnTop,
+  },
+}
 
 -- and finally, return the configuration to wezterm
 return config
