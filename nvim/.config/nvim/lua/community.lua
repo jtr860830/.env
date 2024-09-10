5245dd7d24bb6916bc9cb01a340296e358263b13
Josh Hsieh <dev@josh-hsieh.tw>
2024-09-10 18:22:37 +0800

    feat: add zig support for neovim

--- nvim/.config/nvim/lua/community.lua
+++ nvim/.config/nvim/lua/community.lua
@@ -23,4 +23,5 @@ return {
   { import = "astrocommunity.pack.sql" },
   { import = "astrocommunity.pack.typescript-all-in-one" },
   { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.zig" },
 }
