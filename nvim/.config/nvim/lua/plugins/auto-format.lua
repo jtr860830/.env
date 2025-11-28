89e86845a429232e30553401335df32a3c7aaac1
Josh Hsieh <josh.hsieh@linux.com>
2025-11-28 14:33:48 +0800

    chore: update neovim config

---
+++ nvim/.config/nvim/lua/plugins/auto-format.lua
@@ -0,0 +1,10 @@
+---@type LazySpec
+return {
+  "AstroNvim/astrolsp",
+  ---@type AstroLSPOpts
+  opts = {
+    formatting = {
+      format_on_save = false, -- enable or disable automatic formatting on save
+    },
+  },
+}
