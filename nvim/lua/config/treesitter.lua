a4e17246e1bbd6106af6b37d44ec615e68371889
Josh Hsieh <josh.hsieh@linux.com>
2026-06-01 20:52:28 +0800

    fix: update treesitter config for nvim-treesitter 0.10 API

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -1,4 +1,6 @@
-require("nvim-treesitter.configs").setup({
-  highlight = { enable = true },
-  indent    = { enable = true },
+-- nvim-treesitter 0.10+ delegates highlighting to neovim's built-in treesitter
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function()
+    pcall(vim.treesitter.start)
+  end,
 })
