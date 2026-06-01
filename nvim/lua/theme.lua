6aadfc27f57e035a448b29d0d0eba4ff540f14f9
Josh Hsieh <josh.hsieh@linux.com>
2026-06-01 21:32:59 +0800

    feat: set indent guide colors via onedarkpro palette

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -5,3 +5,7 @@ require("onedarkpro").setup({
 })
 
 vim.cmd.colorscheme("onedark")
+
+local c = require("onedarkpro.helpers").get_colors()
+vim.api.nvim_set_hl(0, "IblIndent",            { fg = c.gray, nocombine = true })
+vim.api.nvim_set_hl(0, "MiniIndentscopeSymbol", { fg = c.blue, nocombine = true })
