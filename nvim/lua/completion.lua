571d69ff9badc01c52f10fb50b350cb2fa1bdd6a
Josh Hsieh <josh.hsieh@linux.com>
2026-06-01 21:05:44 +0800

    feat: configure blink.cmp tab/s-tab/enter keymaps

--- nvim/lua/completion.lua
+++ nvim/lua/completion.lua
@@ -1,5 +1,12 @@
 require("blink.cmp").setup({
-  keymap    = { preset = "default" },
+  keymap = {
+    preset    = "none",
+    ["<Tab>"]   = { "select_next", "snippet_forward", "fallback" },
+    ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+    ["<CR>"]    = { "accept", "fallback" },
+    ["<C-e>"]   = { "hide" },
+    ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
+  },
   appearance = { nerd_font_variant = "mono" },
   sources   = {
     default = { "lsp", "path", "snippets", "buffer" },
