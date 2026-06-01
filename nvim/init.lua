661f667f1baf808d58ef9abff77536de93b0b78e
Josh Hsieh <josh.hsieh@linux.com>
2026-06-01 21:00:06 +0800

    refactor: flatten lua config directory structure

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,11 @@
 vim.g.mapleader      = " "
 vim.g.maplocalleader = " "
 
-require("config.options")
-require("config.theme")
-require("config.keymaps")
-require("config.treesitter")
-require("config.lsp")
-require("config.completion")
-require("config.fzf")
-require("config.plugins")
+require("options")
+require("theme")
+require("keymaps")
+require("treesitter")
+require("lsp")
+require("completion")
+require("fzf")
+require("plugins")
